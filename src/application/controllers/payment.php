efe7956fd87ee0473d52c60cc8f078dee28efbbd
Roy Zheng <roycraft3@gmail.com>
2014-03-08 14:39:43 -0800

    style

--- src/application/controllers/payment.php
+++ src/application/controllers/payment.php
@@ -13,8 +13,8 @@ class Payment extends CI_Controller {
         'returnUrl' => '/payment/complete',
     );
 
-    private static function generateURL($base) {     
-   $url = $base . '?';
+    private static function generateURL($base) {
+        $url = $base . '?';
         foreach (Payment::$creds as $k => $v) {
             $url.=$k . '=' . $v . '&';
         }
@@ -22,17 +22,21 @@ class Payment extends CI_Controller {
     }
 
     public function index($amt) {
-if(isset($amt)){
-        if (!is_numeric($amt)) {
- echo 'Cannot ask to pay for non-numeric value ' . $amt;return;
+        if (isset($amt)) {
+            if (!is_numeric($amt)) {
+                echo 'Cannot ask to pay for non-numeric value ' . $amt;
+                return;
+            }
+            $url = 'https://api-3t.paypal.com/nvp';
+            $url = Payment::generateURL($url) . 'PAYMENTREQUEST_0_PAYMENTACTION=SALE&PAYMENTREQUEST_0_AMT=' . $amt;
+            echo 'url: ' . $url . '<a href="' . $url . '">click</a>';
+            ;
+            return;
         }
-        $url = 'https://api-3t.paypal.com/nvp';
-        $url = Payment::generateURL($url) . 'PAYMENTREQUEST_0_PAYMENTACTION=SALE&PAYMENTREQUEST_0_AMT=' . $amt;
-        echo 'url: '.$url.'<a href="'.$url.'">click</a>';;
-return;
-}
-echo 'payment page<br />';
-echo '<a href="payment/index/5">pay $5</a>';        
+        echo 'payment page<br />';
+        echo '<a href="payment/index/5">pay $5</a>';
+
+        $url = 'https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=tokenValue';
     }
 
     public function cancel() {
