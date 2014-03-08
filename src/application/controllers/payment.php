9c39b92581102b6aca1141b0a34ddd5f9b9bc779
Roy Zheng <yiweizhe@usc.edu>
2014-03-08 10:07:52 +0000

    working on payment system

--- src/application/controllers/payment.php
+++ src/application/controllers/payment.php
@@ -2,7 +2,7 @@
 
 class Payment extends CI_Controller {
 
-    private $creds = array(
+    static $creds = array(
         'USER' => 'platfo_1255077030_biz_api1.gmail.com',
         'PWD' => '1255077037',
         'SIGNATURE' => 'Abg0gYcQyxQvnf2HDJkKtA-p6pqhA1k-KTYE0Gcy1diujFio4io5Vqjf',
@@ -13,25 +13,26 @@ class Payment extends CI_Controller {
         'returnUrl' => '/payment/complete',
     );
 
-    private function generateURL($base) {
-        $url = $base . '?';
-        foreach ($creds as $k => $v) {
+    private static function generateURL($base) {     
+   $url = $base . '?';
+        foreach (Payment::$creds as $k => $v) {
             $url.=$k . '=' . $v . '&';
         }
         return $url;
     }
 
-    public function index() {
-        
-    }
-
-    public function id($amount) {
-        if (!is_int($amount)) {
-            throw new Exception('Cannot ask to pay for noninteger value ' . $amount);
+    public function index($amt) {
+if(isset($amt)){
+        if (!is_numeric($amt)) {
+ echo 'Cannot ask to pay for non-numeric value ' . $amt;return;
         }
         $url = 'https://api-3t.paypal.com/nvp';
-        $url = Payment::generateURL($url) . 'PAYMENTREQUEST_0_PAYMENTACTION=SALE&PAYMENTREQUEST_0_AMT=' . $amount;
-        echo $url;
+        $url = Payment::generateURL($url) . 'PAYMENTREQUEST_0_PAYMENTACTION=SALE&PAYMENTREQUEST_0_AMT=' . $amt;
+        echo 'url: '.$url.'<a href="'.$url.'">click</a>';;
+return;
+}
+echo 'payment page<br />';
+echo '<a href="payment/index/5">pay $5</a>';        
     }
 
     public function cancel() {
