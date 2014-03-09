58eddf54f8020058617defca84dda04a5c263089
Jack Wong <yamijack2000@gmail.com>
2014-03-08 17:59:16 -0800

    Fix PostgreSQL database driver name

--- src/application/config/database.php
+++ src/application/config/database.php
@@ -48,11 +48,11 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost:5432';
+$db['default']['hostname'] = 'localhost';
 $db['default']['username'] = '';
 $db['default']['password'] = '';
 $db['default']['database'] = '';
-$db['default']['dbdriver'] = 'postgresql';
+$db['default']['dbdriver'] = 'postgre';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
