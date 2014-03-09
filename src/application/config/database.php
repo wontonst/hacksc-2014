55032f97afb0bb3442146aa63569ae8c9dbd8bb1
Jack Wong <yamijack2000@gmail.com>
2014-03-08 23:31:23 -0800

    Change database config to use dynamic DNS

--- src/application/config/database.php
+++ src/application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = 'betty.crabdance.com';
 $db['default']['username'] = 'postgres';
 $db['default']['password'] = 'stepforwardfrogproblem';
 $db['default']['database'] = 'betty';
